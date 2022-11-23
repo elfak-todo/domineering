b47e780bc067a2d812d2e66e290f584055e0d813
akimitica <akimitica@elfak.rs>
2022-11-23 15:08:43 +0100

    Dodati komentari sa vezbi

--- game.py
+++ game.py
@@ -2,6 +2,26 @@
 # Player 1 - Uspravno - 0
 # Player 2 - Vodoravno - 1
 
+# Nema potrebe da cuvamo koji igrac je postavio plocicu jer ne utice na ishod igre. 
+# Za stanja je bitno samo da li je polje zauzeto ili ne
+
+
+# Dozvljeno je hradkodiranje pocetne dubine trazenja, 
+# pa da posle dinamicki menjamo kako bismo kontrolisali brzinu igre
+# Obrada stabla stanja preko MiniMax sa alfa beta odsecanjem jer ubrzava igru za 33%
+
+
+"""Reprezentacija stanja na nacin na koji izbegavamo navodnike
+
+class Symbol(object):
+    def __init__(self, name):
+        self.name = name
+    def --repr__(self):
+        return self.name
+"""
+
+
+
 class Game:
     def __init__(self):
         self.board = []
